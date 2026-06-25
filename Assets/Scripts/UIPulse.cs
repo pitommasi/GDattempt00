bc41c2036b00804267467ae537903b89c7554e57
Pierpaolo Tommasi <pierpaolo.tommasi@ext.ec.europa.eu>
2026-06-25 12:52:57 +0200

    Pulsing script added and applied to life icons

---
+++ Assets/Scripts/UIPulse.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class UIPulse : MonoBehaviour {
+    public float pulseSpeed = 3f;
+    public float pulseAmount = 0.15f;
+
+    private Vector3 baseScale;
+
+    private void Awake() {
+        baseScale = transform.localScale;
+    }
+
+    private void Update() {
+        float pulse = 1f + Mathf.Sin(Time.time * pulseSpeed) * pulseAmount;
+
+        transform.localScale = new Vector3(
+            baseScale.x * pulse,
+            baseScale.y * pulse,
+            baseScale.z
+        );
+    }
+}
